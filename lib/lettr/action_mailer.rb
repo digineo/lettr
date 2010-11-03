e06cdc899e84eb21e127af94e41d7d1aeac80112
Timo Göllner <timo.goellner@gmail.com>
2010-11-03 12:28:13 +0100

    Lettr:Mailer debugging

--- lib/lettr/action_mailer.rb
+++ lib/lettr/action_mailer.rb
@@ -8,21 +8,35 @@ module Lettr::ActionMailer
       lettr_request_hash = {}
       lettr_request_hash[:recipient] = recipient
       lettr_request_hash[:subject] = mail.subject
-      mail.parts.each do |part|
-        case part.content_type
+      p mail.encoded
+      p mail.body
+      p mail.parts
+      if mail.parts.empty?
+        case mail.content_type
         when 'text/html'
-          lettr_request_hash[:html] = part.body
+          lettr_request_hash[:html] = mail.body
         when 'text/plain'
-          lettr_request_hash[:text] = part.body
-        else
-          lettr_request_hash[:attachments] ||= []
-          lettr_request_hash[:attachments] << part.body
+          lettr_request_hash[:text] = mail.body
+        end
+      else
+        mail.parts.each do |part|
+          case part.content_type
+          when 'text/html'
+            lettr_request_hash[:html] = part.body
+          when 'text/plain'
+            lettr_request_hash[:text] = part.body
+          else
+            lettr_request_hash[:attachments] ||= []
+            lettr_request_hash[:attachments] << part.body
+          end
         end
       end
       lettr_request_hashes << lettr_request_hash
     end
     identifier = @template
     lettr_request_hashes.each do |lettr_request_hash|
+      p 'identifier: ', identifier
+      p lettr_request_hash
       Lettr.send(identifier, lettr_request_hash).deliver
     end
 
